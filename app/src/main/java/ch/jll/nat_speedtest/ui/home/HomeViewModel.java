dcd22e334b2e2ca1a9d8a0738b1ef2be322ab326
Luzi Martin <luzimartin9@gmail.com>
2020-02-03 15:09:30 +0100

    Added Project Setup

---
+++ app/src/main/java/ch/jll/nat_speedtest/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package ch.jll.nat_speedtest.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
